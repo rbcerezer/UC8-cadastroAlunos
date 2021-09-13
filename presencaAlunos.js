03f2bcb1c9daab305cfd8bf1f8b2390a4e890ea8
rbcerezer <rb.cerezer@gmail.com>
2021-09-13 20:35:36 -0300

    projeto atualizado

--- presencaAlunos.js
+++ presencaAlunos.js
@@ -4,10 +4,10 @@ for (let index = 0; index <= alunos; index ++){
     if (index ==0){
         console.log ("Zero alunos")
     }else if (index % 2 ==0){
-        console.log ("O Número" + index + " Número é Par");{
+        console.log ("O Número " + index + " Número é Par");{
         }
-            }else{
-        console.log ("O Número" + index + " Número é Ímpar")
+    }else{
+        console.log ("O Número " + index + " Número é Ímpar")
     }
 }
 
